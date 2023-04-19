2a1da8758c73a2708bf47fc8edd6849f0f75d7ba
grisha <shopimisss@gmail.com>
2023-04-19 18:52:54 +0300

    created new section, but without "Якщо товар із колекції вже є в кошику - цей товар не виводиться в секції."

---
+++ src/sections/test-featured-products/test-featured-products.js
@@ -0,0 +1,32 @@
+document.body.addEventListener("click", function (event) {
+    const target = event.target.closest("[data-js]");
+    if (!target) return;
+    if (target.dataset.js == "add-to-cart-btn") {
+      event.preventDefault();
+      event.stopPropagation();
+      const formData = new FormData();
+      const variantId = +target.dataset.varId;
+      const urlGet = window.Shopify.routes.root + "cart.js";
+      const urlAdd = window.Shopify.routes.root + "cart/add.js";
+      formData.set("id", variantId);
+      formData.set("quantity", 1);
+      fetch(urlAdd, {
+        method: "POST",
+        body: formData
+      }).then(res => res.json()).then(addData => {
+        fetch(urlGet).then(res => res.json()).then(getData => {
+          const headerCartQty = document.querySelector("#cart-icon-bubble .cart-count-bubble");
+          if (headerCartQty) {
+            headerCartQty.textContent = getData.item_count;
+          } else {
+              let iconWrapper = document.querySelector("#cart-icon-bubble");
+              let iconCount = document.createElement("div");
+              iconCount.classList.add("cart-count-bubble");
+              iconCount.textContent = getData.item_count;
+  
+              iconWrapper.appendChild(iconCount);
+          }
+        });
+      });
+    }
+  });
